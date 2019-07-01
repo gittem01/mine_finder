e76f6d6e66c2814c7436813f916f883b73e88ca9
gittem01 <mchtt43@gmail.com>
2019-07-02 01:10:18 +0300

    Step by step

--- main.py
+++ main.py
@@ -3,37 +3,59 @@ import random
 import numpy as np
 
 windowName = 'Drawing'
-game_size = [12, 12]
+game_size = [2, 2]
+N = 50
 
-def plant_mine(n, area_size): # Returns a list of coordinates which shows the positions of the mines
+class game:
+    def __init__(self, game_size, img):
+        self.game_size = game_size
+        self.mine_list = []
+        self.img = img
 
-    mine_list = []
+    def plant_mine(self, n, area_size): # Returns a list of coordinates which includes the positions of the mines
 
-    def give_coord(AS):
-        coordx = random.randrange(AS[0])
-        coordy = random.randrange(AS[1])
-        return [coordx, coordy]
+        def give_coord(AS):
+            coordx = random.randrange(AS[0])
+            coordy = random.randrange(AS[1])
+            return [coordx, coordy]
 
-    while len(mine_list) < n:
-        coord = give_coord(area_size)
-        if coord not in mine_list:
-            mine_list.append(coord)
-        else:
-            continue
-    return mine_list
+        while len(self.mine_list) < n:
+            coord = give_coord(area_size)
+            if coord not in self.mine_list:
+                self.mine_list.append(coord)
+            else:
+                continue
 
-def draw_circle(event, x, y, flags, param):
+    def draw_game(self):
+        for i in range(1, self.game_size[0]):
+            cv2.line(self.img, (N * i, 0), (N * i, N * self.game_size[1]),
+                                                             (255,0,0),2)
+
+        for i in range(1, self.game_size[1]):
+            cv2.line(self.img, (0, N * i), (N * self.game_size[0], N * i),
+                                                             (255,0,0),2)
+
+    def define_square(self, coord):
+        pass # This function will find the simple coordinate from complex one
+
+    def def_square_coords(self, coord):
+        pass # This function will find the covering area of the square according to the point
+
+def event_func(event, x, y, flags, param):
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, (x,y), 10, (255, 0, 0), -1)
+        cv2.circle(img, (x,y), 1, (255, 0, 0), -1)
         #Do smth else in here
 
+img = np.zeros((game_size[0]*N,game_size[1]*N, 3), np.uint8)
 
-
-
-img = np.zeros((game_size[0]*50,game_size[1]*50, 3), np.uint8)
 cv2.namedWindow(windowName)
-cv2.setMouseCallback(windowName, draw_circle)
+cv2.setMouseCallback(windowName, event_func)
+
+the_game = game(game_size, img)
+the_game.draw_game()
+
+the_game.img[40:60, 40:60] = 0
 
 while (True):
     cv2.imshow(windowName, img)
