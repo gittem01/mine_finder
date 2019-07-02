fae262d1c73670f966c4cf73421babb098d23d34
gittem01 <mchtt43@gmail.com>
2019-07-02 17:49:23 +0300

    Getting closerrrr

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 
 windowName = 'Drawing'
-game_size = [2, 2]
+game_size = [8, 8]
 N = 50
 
 class game:
@@ -27,24 +27,24 @@ class game:
                 continue
 
     def draw_game(self):
-        for i in range(1, self.game_size[0]):
+        for i in range(self.game_size[0] + 1):
             cv2.line(self.img, (N * i, 0), (N * i, N * self.game_size[1]),
                                                              (255,0,0),2)
 
-        for i in range(1, self.game_size[1]):
+        for i in range(self.game_size[1] + 1):
             cv2.line(self.img, (0, N * i), (N * self.game_size[0], N * i),
                                                              (255,0,0),2)
 
     def define_square(self, coord):
-        pass # This function will find the simple coordinate from complex one
-
-    def def_square_coords(self, coord):
-        pass # This function will find the covering area of the square according to the point
+        coordx = (coord[0]//N) * N
+        coordy = (coord[1]//N) * N
+        self.img[coordy + 2:coordy + N - 1, coordx + 2:coordx + N - 1] = 255
 
 def event_func(event, x, y, flags, param):
 
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img, (x,y), 1, (255, 0, 0), -1)
+        the_game.define_square([x, y])
         #Do smth else in here
 
 img = np.zeros((game_size[0]*N,game_size[1]*N, 3), np.uint8)
@@ -55,10 +55,8 @@ cv2.setMouseCallback(windowName, event_func)
 the_game = game(game_size, img)
 the_game.draw_game()
 
-the_game.img[40:60, 40:60] = 0
-
 while (True):
-    cv2.imshow(windowName, img)
+    cv2.imshow(windowName, the_game.img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
